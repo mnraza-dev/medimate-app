2dd6390e88080fdb5b351982bfe28e327ca2d75e
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-29 04:48:26 +0530

    added Splash screen

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+
+export default function SplashScreen() {
+    return (
+        <View>
+            <Text style={{ color: 'white' }}>SplashScren</Text>
+        </View>
+    )
+}
